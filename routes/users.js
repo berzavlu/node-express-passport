a86bfbc35d7c9ea266b8ab8803a0f6fad70279fb
Luis Eduardo Berrospi <berzavlu@gmail.com>
2020-02-25 01:15:38 -0500

    vista bootstrap fix

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,20 @@ router.get('/register', (_, res) => res.render('register'))
 
 router.post('/register', (req, res) => {
   const { name, email, password } = req.body
-  const newUser = new User({ name, email, password })
 
-  newUser
-    .save()
-    .then(() => res.sendStatus(200))
+  User.findOne({ email })
+    .then((objUser) => {
+      // si no existe lo registro
+      if (!objUser) {
+        const newUser = new User({ name, email, password })
+        newUser
+          .save()
+          .then(() => res.sendStatus(200))
+          .catch(() => res.sendStatus(500))
+      } else {
+        res.sendStatus(500)
+      }
+    })
     .catch(() => res.sendStatus(500))
 })
 
